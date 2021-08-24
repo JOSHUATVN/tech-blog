719c2ac301890c554951450d11d3e54a6bc05d1a
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-08-24 17:13:43 -0500

    fixed server starting issues

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const path = require('path');
-const exphbs = require('express=handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helper = require('./utils/helpers');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const SequelizeStore = require('connect-session-sequilize')(session.Store);
-
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
@@ -21,7 +18,6 @@ const sess = {
         db: sequelize
     })
 };
-
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
